24f0fd8bd3407b16216d73b59d6f381e60f34c01
Ultramarine1939-syujie <2021218017@mail.hfut.edu.cn>
2023-08-19 16:48:10 +0800

    1

---
+++ src/rplidar_control/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/offboard_control/src/rplidar_control/src
+# Build directory: /home/ubuntu/offboard_control/src/rplidar_control/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ src/rplidar_control/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/offboard_control/src/rplidar_control/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/offboard_control/devel;/home/ubuntu/repos/realsense_mavros/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/offboard_control/devel;/home/ubuntu/repos/realsense_mavros/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic' != '' else []
